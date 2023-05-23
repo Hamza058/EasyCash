42c3dfa657d0bb1c7484f09c6c1faefc1b0d8b69
hamza kılıç <hamza2358@hotmail.com>
2023-05-23 17:28:36 +0300

    confirm mail fixes

--- EasyCashIdentity.PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentity.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -1,19 +1,34 @@
-﻿using EasyCashIdentity.PresentationLayer.Models;
+﻿using EashCashIdentity.EntityLayer.Concrete;
+using EasyCashIdentity.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentity.PresentationLayer.Controllers
 {
 	public class ConfirmMailController : Controller
 	{
-		[HttpGet]
+		private readonly UserManager<AppUser> _userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
 		public IActionResult Index(int id)
 		{
 			return View();
 		}
 
         [HttpPost]
-        public IActionResult Index(ConfirmMailViewModel confirmMailViewModel)
+        public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel, string mail)
         {
+			var value = mail;
+            var user = await _userManager.FindByEmailAsync(value.ToString());
+            if(user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+            {
+                return RedirectToAction("Index", "MyProfile");
+            }
             return View();
         }
     }
